2d9f1a4aa4ab7fb059c9502f1302ebd14190de32
James Palmer <james.palmer@itv.com>
2024-05-06 11:48:39 +0100

    join

--- src/main/scala/Econcurrency/Join.scala
+++ src/main/scala/Econcurrency/Join.scala
@@ -1,6 +1,7 @@
 package Econcurrency
 
 import cats.effect._
+import cats.effect.std.Queue
 import fs2._
 
 import scala.concurrent.duration._
@@ -27,5 +28,24 @@ object Join extends IOApp.Simple{
     // bounded
     val jBounded = Stream(s1Finite, s2Finite, s3Infinite).parJoin(2)
     jBounded.printlns.interruptAfter(3.seconds).compile.drain
+
+    // exercise
+    def producer(id: Int, queue: Queue[IO, Int]): Stream[IO, Nothing] = {
+      Stream.repeatEval(queue.offer(id)).drain
+    }
+
+    def consumer(id: Int, queue: Queue[IO, Int]): Stream[IO, Nothing] = {
+      Stream.repeatEval(queue.take).map(i => s"Consuming message $i from consumer $id").printlns
+    }
+
+    // create stream that emits the queue, use queue to create 4 producers and 10 consumers with sequential ids
+    // run the producers and consumers in paralell, finish after 5 seconds
+
+    Stream.eval(Queue.unbounded[IO, Int]).flatMap { q =>
+      val ps: Stream[Pure, Stream[IO, Nothing]] = Stream.range(0, 5).map(id => producer(id, q))
+      val cs: Stream[Pure, Stream[IO, Nothing]] = Stream.range(0, 10).map(id => consumer(id, q))
+      val all = ps ++ cs
+      all.parJoinUnbounded
+    }.interruptAfter(5.seconds).compile.drain
   }
 }
